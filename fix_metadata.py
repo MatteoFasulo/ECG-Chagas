4b70bc0b1203140c7b836545cec79015252ab27d
Matteo Fasulo <mat.fasulo@gmail.com>
2025-02-13 19:18:37 +0100

    Enhance slideshow metadata handling: set slide types for headers and default to 'skip'

--- fix_metadata.py
+++ fix_metadata.py
@@ -13,12 +13,22 @@ if __name__ == '__main__':
 
     # Modify all cells
     for cell in nb.cells:
+        # Add metadata if it doesn't exist
         if 'metadata' not in cell:
             cell['metadata'] = {}
+        # Add slideshow metadata
         cell['metadata']['slideshow'] = {'slide_type': 'skip'}
+
+        # If the cell is a first level header, set it to slide type 'slide'
+        if cell.cell_type == 'markdown' and cell.source.startswith('# '):
+            cell['metadata']['slideshow']['slide_type'] = 'slide'
+        
+        # If the cell is a second level header or third level header, set it to slide type 'subslide'
+        if cell.cell_type == 'markdown' and (cell.source.startswith('## ') or cell.source.startswith('### ')):
+            cell['metadata']['slideshow']['slide_type'] = 'subslide'
 
     # Save the modified notebook
     with open(notebook_path, 'w', encoding='utf-8') as f:
         nbformat.write(nb, f)
 
-    print("All cells have been set to slide type 'skip'")
+    print("All cells have been set to slide type 'skip', first level headers to 'slide', and second/third level headers to 'subslide'")
